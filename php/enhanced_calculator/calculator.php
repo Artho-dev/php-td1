c3e83d498fc8c95517c6f0406cd765d017914a87
Artho-dev <loic.ganne@etu.univ-amu.fr>
2020-12-18 10:25:58 +0100

    exo 6 finis

---
+++ php/enhanced_calculator/calculator.php
@@ -0,0 +1,39 @@
+<?php include('myLib.php'); ?>
+
+<?php start_page("Calculatrice") ?>
+
+    <h1> Calculatrice </h1>
+
+    <form action="calcul.php" method="post">
+        <div>
+            <label for="op1"> Première Valeur: </label>
+            <input type="text" id="op1" name="op1" required
+                   minlength="1">
+            <div>
+                <?php $operateurs = '*+-/'; ?>
+                <?php
+                    for ($i = 0; $i < strlen($operateurs); ++$i ) {
+                        echo '<input ';
+                        if($i == 0)
+                        {
+                            echo 'checked="checked" ';
+                        }
+                        echo 'type="radio" name="op" value="' . $operateurs[$i] . '"/>' . $operateurs[$i] . ' <br/>' . "\n";
+                    }
+                ?>
+            </div>
+
+            <label for="op2"> Deuxième Valeur: </label>
+            <input type="text" id="op2" name="op2" required
+                   minlength="1">
+        </div>
+
+        <hr>
+
+        <button class="favorite styled"
+                type="submit">
+            Calculer
+        </button>
+
+    </form>
+<?php end_page() ?>
